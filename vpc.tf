c7da0810388407312f178a913eeaeeb9d61a550f
Asad Hanif <asadhanif3188@gmail.com>
2024-01-19 12:23:22 +0500

    security_group added

--- vpc.tf
+++ vpc.tf
@@ -11,3 +11,25 @@ module "vpc" {
   single_nat_gateway  = false
   enable_vpn_gateway  = false
 }
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "${var.prefix}rds-sg"
+  description = "RDS MySQL security group"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      description = "MySQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+
+  tags = local.tags
+}
